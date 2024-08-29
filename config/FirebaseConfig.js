a04ff264a66d090c74a3a0e05dcc93715cbfb273
ayhman <wearecoder786@gmail.com>
2024-08-29 14:04:20 +0530

    add .env file for firebase config

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -4,19 +4,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDTn6tg5-ijXW6kBvAjjU0mH0yj7rnSruQ",
-  authDomain: "ai-travel-planner-97e0c.firebaseapp.com",
-  projectId: "ai-travel-planner-97e0c",
-  storageBucket: "ai-travel-planner-97e0c.appspot.com",
-  messagingSenderId: "871150511268",
-  appId: "1:871150511268:web:3a87775f2b4e2724e17231",
-  measurementId: "G-4WJV8TJDW8",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
